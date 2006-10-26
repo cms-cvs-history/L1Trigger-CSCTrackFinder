e09434c8eb2bac90cb3be0d55fe312393ae23d3f
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-10-26 23:57:59 +0000

    Add missing ';' to macro use

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -3,5 +3,5 @@
 #include "L1Trigger/CSCTrackFinder/src/CSCTFTrackProducer.h"
 #include "L1Trigger/CSCTrackFinder/src/CSCTFCandidateProducer.h"
 
-DEFINE_FWK_MODULE(CSCTFTrackProducer)
-  DEFINE_ANOTHER_FWK_MODULE(CSCTFCandidateProducer)
+DEFINE_FWK_MODULE(CSCTFTrackProducer);
+DEFINE_ANOTHER_FWK_MODULE(CSCTFCandidateProducer);
