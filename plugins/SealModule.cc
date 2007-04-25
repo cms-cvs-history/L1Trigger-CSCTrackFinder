bfad8be301ac742797f7a6339e8d19ea29b330e8
Khristian Kotov <sha1-eaa78e6d3e4d95dc95db05db8a523e5e7ca58c4b@cern.ch>
2007-04-25 21:52:21 +0000

    Requirements of new plugin manager is now satisfied for this package

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -1,7 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "L1Trigger/CSCTrackFinder/src/CSCTFTrackProducer.h"
-#include "L1Trigger/CSCTrackFinder/src/CSCTFCandidateProducer.h"
+#include "L1Trigger/CSCTrackFinder/plugins/CSCTFTrackProducer.h"
+#include "L1Trigger/CSCTrackFinder/plugins/CSCTFCandidateProducer.h"
 
 DEFINE_FWK_MODULE(CSCTFTrackProducer);
 DEFINE_ANOTHER_FWK_MODULE(CSCTFCandidateProducer);
